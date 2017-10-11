a544e02a8c5c219358667c448bce93012f226362
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-10-11 14:22:28 +0900

    Initial commit

---
+++ CaluculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaluculatorApp
+//
+//  Created by masataka kono on 2017/10/11.
+//  Copyright © 2017年 masataka.kono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CaluculatorAppTests/CaluculatorAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CaluculatorAppTests.swift
+//  CaluculatorAppTests
+//
+//  Created by masataka kono on 2017/10/11.
+//  Copyright © 2017年 masataka.kono. All rights reserved.
+//
+
+import XCTest
+@testable import CaluculatorApp
+
+class CaluculatorAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
